97be499a1eaf7a5405eb0bc89604e870db8fd3b1
KTshnikk <154802999+KTshnikk@users.noreply.github.com>
2024-11-12 00:25:19 +0700

    feat: added validation schemas

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ProfileForm } from "@/components/ui/schema"
+
+export default function ProfilePage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1></h1>
+      <ProfileForm />
+    </div>
+  )
+}
